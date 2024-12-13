3186350d4c8e9053a7f77d8c8112c93b506549a1
김찬희 <blackeagle10@icloud.com>
2024-12-13 15:40:38 +0900

    tag lib .jar 파일 추가

--- src/main/java/com/khedu/sooljura/admin/model/dao/AdminDao.java
+++ src/main/java/com/khedu/sooljura/admin/model/dao/AdminDao.java
@@ -1,6 +1,5 @@
 package com.khedu.sooljura.admin.model.dao;
 
-import jdk.jfr.Registered;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;

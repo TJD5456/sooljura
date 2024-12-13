b6f23af35a95027f55aea0bf0f7762aea6e75e80
김찬희 <blackeagle10@icloud.com>
2024-12-13 17:32:32 +0900

    sql 오타 수정

--- Oracle DB SQL/soojura.sql
+++ Oracle DB SQL/soojura.sql
@@ -3,14 +3,14 @@
 
 drop table tbl_product;
 
-drop sequence seq_dinner;
+drop sequence seq_product;
 
 create table tbl_product
 (
 
 );
 
--- 'p' || to_char(sysdate, 'yymmdd') || lpad (seq_dinner.nextval, 4, '0')
+-- 'p' || to_char(sysdate, 'yymmdd') || lpad (seq_product.nextval, 4, '0')
 create sequence seq_product maxvalue 9999 cycle;
 
 commit;

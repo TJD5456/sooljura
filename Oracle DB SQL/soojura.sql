f0d1c025a529538e5413a774cc12126885a589c0
김찬희 <blackeagle10@icloud.com>
2024-12-13 17:30:29 +0900

    ㅡㅏ

--- Oracle DB SQL/soojura.sql
+++ Oracle DB SQL/soojura.sql
@@ -1,33 +1,16 @@
 -- user: sooljura
 -- password: 1234
 
--- 코드 관리 예시
-create table
-    tbl_code_2 (
-        group_cd varchar2 (10), -- 대분류 코드
-        group_cd_nm varchar2 (100), -- 대분류 코드명
-        src_cd varchar2 (10), -- 중분류 코드
-        src_cd_nm varchar2 (100), -- 중분류 코드명
-        tar_cd varchar2 (10), -- 소분류 코드
-        tar_cd_nm varchar2 (100) -- 소분류 코드명
-    );
+drop table tbl_product;
 
-insert into tbl_code_2 values ('A0001', '제품관리', 'B1', '주종관리', '1', '위스키');
-insert into tbl_code_2 values ('A0001', '제품관리', 'B1', '주종관리', '2', '소주');
-insert into tbl_code_2 values ('A0001', '제품관리', 'B1', '주종관리', '3', '증류주');
-insert into tbl_code_2 values ('A0001', '제품관리', 'B1', '주종관리', '4', '맥주');
+drop sequence seq_dinner;
 
-insert into tbl_code_2 values ('A0001', '제품관리', 'B2', '제조사관리', '1', '삼성');
-insert into tbl_code_2 values ('A0001', '제품관리', 'B2', '제조사관리', '2', '엘지');
-insert into tbl_code_2 values ('A0001', '제품관리', 'B2', '제조사관리', '3', '카카오');
-insert into tbl_code_2 values ('A0001', '제품관리', 'B2', '제조사관리', '4', '네이버');
+create table tbl_product
+(
 
-insert into tbl_code_2 values ('B0001', '게시글관리', 'A1', '글종류', '1', '공지사항');
-insert into tbl_code_2 values ('B0001', '게시글관리', 'A1', '글종류', '2', 'FAQ');
-insert into tbl_code_2 values ('B0001', '게시글관리', 'A1', '글종류', '3', '자유게시판');
-insert into tbl_code_2 values ('B0001', '게시글관리', 'A1', '글종류', '4', '질문게시판');
+);
+
+-- 'p' || to_char(sysdate, 'yymmdd') || lpad (seq_dinner.nextval, 4, '0')
+create sequence seq_product maxvalue 9999 cycle;
 
 commit;
-
-select * from tbl_code_2
-order by group_cd, src_cd, tar_cd;
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,10 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
          pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>index.jsp</title>
+    <script src="/resources/jquery/jquery-3.7.1.min.js"></script>
 </head>
 <body>
 <h1>메인페이지</h1>

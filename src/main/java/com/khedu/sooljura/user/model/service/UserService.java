1a3f57ae4a52a62f7b3727a07caf413a0c530d65
DESKTOP-LIHUIGB\user1 <bin900902@gmail.com>
2024-12-17 11:46:40 +0900

    pom.xml -> BCrypt 추가 user-mapper.xml -> member-mapper.xml 이름 변경, login 쿼리문 작성 mybatis-config -> alias로 user 추가 User, UserService, UserDao, UserController 추가 + 로그인 부분 작성

---
+++ src/main/java/com/khedu/sooljura/user/model/service/UserService.java
@@ -0,0 +1,36 @@
+package com.khedu.sooljura.user.model.service;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.khedu.sooljura.user.model.dao.UserDao;
+import com.khedu.sooljura.user.model.vo.User;
+
+@Service("userService")
+public class UserService {
+	@Autowired
+	@Qualifier("userDao")
+	private UserDao dao;
+
+	public User login(User user) {
+		 User loginUser = dao.login(user);
+		 
+		 if(loginUser == null) {
+			 //일치하는 아이디 없음
+			 return null;
+		 }else {		
+			 //비밀번호 bcrypt로 확인
+			 boolean chkpw = BCrypt.checkpw(user.getUserPw(), loginUser.getUserPw());
+			 
+			 if(chkpw) {
+				 //일치하면 넘겨주기
+				 return loginUser;
+			 }else {
+				 //비밀번호 다르면 null
+				 return null;
+			 }
+		 }
+	} 
+}

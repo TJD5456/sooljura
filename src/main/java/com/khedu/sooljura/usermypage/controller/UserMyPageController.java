51c638433de9c74001804e27c33cf0b38379b4e8
엄태호 <oum96@naver.com>
2024-12-17 19:16:40 +0900

    common.css, footer, header, sidebar, index 수정 및  마이페이지 implement

--- src/main/java/com/khedu/sooljura/usermypage/controller/UserMyPageController.java
+++ src/main/java/com/khedu/sooljura/usermypage/controller/UserMyPageController.java
@@ -20,4 +20,20 @@ public class UserMyPageController {
     public String userPage() {
         return "userMyPage/userMyPage";
     }
+	@GetMapping("userInfo.do")
+    public String userInfo() {
+        return "userMyPage/userInfo";
+    }
+	@GetMapping("shoppingInfo.do")
+    public String shoppingInfo() {
+        return "userMyPage/shoppingInfo";
+    }
+	@GetMapping("readMyPost.do")
+    public String readMyPost() {
+        return "userMyPage/readMyPost";
+    }
+	@GetMapping("likedMerc.do")
+    public String likedMerc() {
+        return "userMyPage/likedMerc";
+    }
 }

543457da4d7b25413e05e6020a8c992a8b194e40
김찬희 <blackeagle10@icloud.com>
2024-12-20 10:04:14 +0900

    se.sql 간단한 정리

--- Oracle DB SQL/sooljura-SE.sql
+++ Oracle DB SQL/sooljura-SE.sql
@@ -1,23 +1,28 @@
-/*
-DROP TABLE tbl_discount_hist;
-DROP TABLE tbl_discount;
-DROP TABLE tbl_basket;
-DROP TABLE tbl_basket_type;
-DROP TABLE tbl_youtube;
-DROP TABLE tbl_product_image;
-DROP TABLE tbl_order_history;
-DROP TABLE tbl_product;
-DROP TABLE tbl_product_category;
-DROP TABLE tbl_user_addr;
-DROP TABLE tbl_comment;
-DROP TABLE tbl_post;
-DROP TABLE tbl_post_type;
-DROP TABLE tbl_chat;
-DROP TABLE tbl_user;
-DROP TABLE tbl_user_type;
-*/
+drop table tbl_discount_hist;
+drop table tbl_discount;
+drop table tbl_basket;
+drop table tbl_basket_type;
+drop table tbl_youtube;
+drop table tbl_product_image;
+drop table tbl_order_history;
+drop table tbl_product;
+drop table tbl_product_category;
+drop table tbl_user_addr;
+drop table tbl_comment;
+drop table tbl_post;
+drop table tbl_post_type;
+drop table tbl_chat;
+drop table tbl_user;
+drop table tbl_user_type;
 
---ReadMe: 주석처리된 테이블 (tbl_order_history, table tbl_youtube, tbl_basket, tbl_discount_hist) 는 tbl_product 미존재로 인해 생성 대기.
+drop sequence seq_user;
+drop sequence seq_user_addr;
+drop sequence seq_post;
+drop sequence seq_comment;
+drop sequence seq_order_history;
+drop sequence seq_basket;
+
+-- 주석처리된 테이블 (tbl_order_history, table tbl_youtube, tbl_basket, tbl_discount_hist) 는 tbl_product 미존재로 인해 생성 대기.
 
 create table tbl_user_type (
    user_cd number primary key,
@@ -26,8 +31,7 @@ create table tbl_user_type (
 
 create table tbl_user (
    user_key    char(11) primary key,
-   user_cd     number not null
-      references tbl_user_type ( user_cd ),
+   user_cd     number not null references tbl_user_type ( user_cd ),
    user_id     varchar2(30) not null,
    user_pw     varchar2(60) not null,
    user_email  varchar2(100) not null,
@@ -49,11 +53,8 @@ create table tbl_post_type (
 
 create table tbl_post (
    post_key        char(11) primary key,
-   post_cd         number not null
-      references tbl_post_type ( post_cd ),
-   user_key        char(11) not null
-      references tbl_user ( user_key )
-         on delete cascade,
+   post_cd         number not null references tbl_post_type ( post_cd ),
+   user_key        char(11) not null references tbl_user ( user_key ) on delete cascade,
    comment_content varchar2(2000) not null,
    post_date       date default sysdate,
    confirm_yn      number default 0,
@@ -61,17 +62,14 @@ create table tbl_post (
    delete_reason   varchar2(200)
 );
 
---게시글 코드 product랑 겹쳐서 's'로 생성.
+-- 게시글 코드 product랑 겹쳐서 's'로 생성.
 -- 's' || lpad(seq_post .nextval, 4, '0')
 create sequence seq_post maxvalue 9999 cycle;
 
 create table tbl_comment (
    comment_key     char(11) primary key,
-   post_key        char(11) not null
-      references tbl_post ( post_key ),
-   user_key        char(11) not null
-      references tbl_user ( user_key )
-         on delete cascade,
+   post_key        char(11) not null references tbl_post ( post_key ),
+   user_key        char(11) not null references tbl_user ( user_key ) on delete cascade,
    comment_content varchar2(2000) not null,
    comment_date    date default sysdate
 );
@@ -81,9 +79,7 @@ create sequence seq_comment maxvalue 9999 cycle;
 
 create table tbl_user_addr (
    addr_key    char(11) primary key,
-   user_key    char(11) not null
-      references tbl_user ( user_key )
-         on delete cascade,
+   user_key    char(11) not null references tbl_user ( user_key ) on delete cascade,
    addr_nm     char(30) not null,
    addr_code   char(5) not null,
    addr        varchar2(300) not null,
@@ -98,27 +94,25 @@ create sequence seq_user_addr maxvalue 9999 cycle;
 
 /* 채팅 테이블 없음 */
 
+/* 상품 테이블 필요 */
+
 /* 상품 카테고리 테이블 필요 */
 
-/* 상품 테이블 필요 */
+/* 상품 이미지 테이블  필요 */
 
 /*
 create table tbl_order_history (
    imp_uid       varchar2(255) primary key,
-   order_no      char(11) UNIQUE NOT NULL,
-   prod_key      char(11) not null
-      references tbl_product ( prod_key ),
-   user_key      char(11) not null
-      references tbl_user ( user_key )
-         on delete cascade,
-   addr_key   char(11) not null
-      references tbl_user_addr ( addr_key ),
+   order_no      char(11) unique not null,
+   prod_key      char(11) not null references tbl_product ( prod_key ),
+   user_key      char(11) not null references tbl_user ( user_key ) on delete cascade,
+   addr_key      char(11) not null references tbl_user_addr ( addr_key ),
    card_company  varchar2(50) not null,
    order_price   number,
    order_cnt     number,
    ordered_date  date default sysdate,
    refund_yn     number default 0,
-   refund_reason varchar2(100) default 'Not satisfied',
+   refund_reason varchar2(100) default '맛없음',
    refund_key    char(11),
    refund_date   date
 );
@@ -127,17 +121,11 @@ create table tbl_order_history (
 -- 'o' || lpad(seq_order_history.nextval, 4, '0')
 create sequence seq_order_history maxvalue 9999 cycle;
 
-/* 상품 이미지 테이블  필요 */
-
-/* 상품 카테고리 테이블 필요 */
-
 /*
 create table tbl_youtube (
    youtube_url varchar2(100) primary key,
    content     varchar2(1000) not null,
-   prod_key    char(11) not null
-     references tbl_product ( prod_key )
-        on delete cascade
+   prod_key    char(11) not null references tbl_product ( prod_key ) on delete cascade
 );
 */
 
@@ -149,13 +137,9 @@ create table tbl_basket_type (
 /*
 create table tbl_basket (
    basket_key char(11) primary key,
-   basket_cd  number not null
-      references tbl_basket_type ( basket_cd ),
-   prod_key   char(11) not null
-      references tbl_product ( prod_key ),
-   user_key   char(11) not null
-      references tbl_user ( user_key )
-         on delete cascade,
+   basket_cd  number not null references tbl_basket_type ( basket_cd ),
+   prod_key   char(11) not null references tbl_product ( prod_key ),
+   user_key   char(11) not null references tbl_user ( user_key ) on delete cascade,
    basket_cnt number default 0
 );
 */
@@ -174,11 +158,8 @@ create table tbl_discount (
 /*
 create table tbl_discount_hist (
    hist_cd    number primary key,
-   prod_key   char(11) not null
-      references tbl_product ( prod_key )
-         on delete cascade,
-   event_cd   number not null
-      references tbl_discount ( event_cd ),
+   prod_key   char(11) not null references tbl_product ( prod_key ) on delete cascade,
+   event_cd   number not null references tbl_discount ( event_cd ),
    start_date date,
    end_date   date
 );

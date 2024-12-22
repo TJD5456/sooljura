ce4a1ece3c4fe2c60f2049f2fc3111f8e04c719c
UnEmotioneD <blackeagle10@icloud.com>
2024-12-22 09:47:45 +0900

    쓰지 않는 import 제거

--- src/main/java/com/khedu/sooljura/user/controller/UserController.java
+++ src/main/java/com/khedu/sooljura/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.khedu.sooljura.user.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
--- src/main/java/com/khedu/sooljura/webscraper/model/service/WebScraperService.java
+++ src/main/java/com/khedu/sooljura/webscraper/model/service/WebScraperService.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
-import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;

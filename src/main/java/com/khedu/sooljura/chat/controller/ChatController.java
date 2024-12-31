1b0ef3239af558d18f522b26fe11cddb3808a657
김찬희 <blackeagle10@icloud.com>
2024-12-31 10:18:50 +0900

    startChat.jsp 에서 controller 로 제목, 내용, userId 전송

--- src/main/java/com/khedu/sooljura/chat/controller/ChatController.java
+++ src/main/java/com/khedu/sooljura/chat/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.khedu.sooljura.chat.controller;
 
 import com.khedu.sooljura.chat.model.service.ChatService;
+import com.khedu.sooljura.chat.model.vo.Chat;
 import com.khedu.sooljura.chat.model.vo.Room;
 import com.khedu.sooljura.user.model.vo.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -22,6 +24,23 @@ public class ChatController {
     @Qualifier("chatService")
     private ChatService service;
 
+    @GetMapping("/startChat.do")
+    public String startChat(HttpSession session, Model model) {
+        User loginUser = (User) session.getAttribute("loginUser");
+        String userId = loginUser.getUserId();
+        model.addAttribute("userId", userId);
+        return "chat/startChat";
+    }
+
+    @PostMapping("/createChat.do")
+    @ResponseBody
+    public String createChat(Room room, Chat chat) {
+
+        int createRoomResult = service.createRoom(room);
+
+        return "";
+    }
+
     //채팅방 목록 조회
     @GetMapping("/getRoomList.do")
     public String getRoomList(Model model, HttpSession session) {
@@ -31,26 +50,6 @@ public class ChatController {
         model.addAttribute("roomList", roomList);
 
         return "chat/roomList";
-    }
-
-    @GetMapping("/getChatList.do")
-    public String createRoomFrm(HttpSession session, Model model) {
-        User loginUser = (User) session.getAttribute("loginUser");
-        String userId = loginUser.getUserId();
-
-//        ArrayList<Chat> chatList = service.selectChatList(userId);
-//        model.addAttribute("chatList", chatList);
-
-        return "chat/chatList";
-    }
-
-    //방 만들기
-    @GetMapping("/createRoom.do")
-    @ResponseBody
-    public String createRoom(HttpSession session, String roomName, String members) {
-        User loginUser = (User) session.getAttribute("loginUser");
-
-        return service.createRoom(roomName, loginUser.getUserId(), members);
     }
 
 }
---
+++ src/main/webapp/WEB-INF/views/chat/startChat.jsp
@@ -0,0 +1,43 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>1:1 질문</title>
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/common.css"/>
+    <script src="${pageContext.request.contextPath}/resources/jquery/jquery-3.7.1.min.js"></script>
+    <style>
+        main {
+            display: flex;
+        }
+
+        #msgInput {
+            margin-top: 16px;
+            margin-bottom: 16px;
+        }
+
+        #msgInput, #roomTitleInput,[type="submit"] {
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+<main>
+    <div class="wrapper">
+        <div class="content">
+            <div class="title">
+                <h1>1:1 질문</h1>
+            </div>
+            <form action="${pageContext.request.contextPath}/chat/createChat.do" method="post">
+                <input type="hidden" name="userId" value="${senderId}">
+                <input type="hidden" name="senderId" value="${senderId}">
+                <label for="roomTitleInput"></label><input type="text" name="roomTitle" id="roomTitleInput" placeholder="제목">
+                <br>
+                <label for="msgInput"></label><textarea name="msg" id="msgInput" rows="10" cols="50" maxlength="1000" wrap="soft" style="resize: none" placeholder="질문"></textarea>
+                <br>
+                <input type="submit" value="보내기">
+            </form>
+        </div>
+    </div>
+</main>
+</body>
+</html>

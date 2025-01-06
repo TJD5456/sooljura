45c81b0e9c52079145b16fa609701a5eb4344140
조용운 <dyddns7731@naver.com>
2025-01-06 20:52:53 +0900

    OrderHistory -> 환불관련 부분 삭제

--- src/main/java/com/khedu/sooljura/product/model/vo/OrderHistory.java
+++ src/main/java/com/khedu/sooljura/product/model/vo/OrderHistory.java
@@ -10,144 +10,102 @@ public class OrderHistory {
     private int orderPrice;
     private int orderCnt;
     private String orderDate;
-    private int refundYn;
-    private String refundReason;
-    private String refundKey;
-    private String refundDate;
-
+	
     public OrderHistory() {
-        super();
-    }
+		super();
+	}
 
-    public OrderHistory(String impUid, String orderNo, String prodKey, String userKey, String addrKey,
-                        String cardCompany, int orderPrice, int orderCnt, String orderDate, int refundYn, String refundReason,
-                        String refundKey, String refundDate) {
-        super();
-        this.impUid = impUid;
-        this.orderNo = orderNo;
-        this.prodKey = prodKey;
-        this.userKey = userKey;
-        this.addrKey = addrKey;
-        this.cardCompany = cardCompany;
-        this.orderPrice = orderPrice;
-        this.orderCnt = orderCnt;
-        this.orderDate = orderDate;
-        this.refundYn = refundYn;
-        this.refundReason = refundReason;
-        this.refundKey = refundKey;
-        this.refundDate = refundDate;
-    }
+	public OrderHistory(String impUid, String orderNo, String prodKey, String userKey, String addrKey,
+			String cardCompany, int orderPrice, int orderCnt, String orderDate) {
+		super();
+		this.impUid = impUid;
+		this.orderNo = orderNo;
+		this.prodKey = prodKey;
+		this.userKey = userKey;
+		this.addrKey = addrKey;
+		this.cardCompany = cardCompany;
+		this.orderPrice = orderPrice;
+		this.orderCnt = orderCnt;
+		this.orderDate = orderDate;
+	}
 
-    public String getImpUid() {
-        return impUid;
-    }
+	public String getImpUid() {
+		return impUid;
+	}
 
-    public void setImpUid(String impUid) {
-        this.impUid = impUid;
-    }
+	public void setImpUid(String impUid) {
+		this.impUid = impUid;
+	}
 
-    public String getOrderNo() {
-        return orderNo;
-    }
+	public String getOrderNo() {
+		return orderNo;
+	}
 
-    public void setOrderNo(String orderNo) {
-        this.orderNo = orderNo;
-    }
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
+	}
 
-    public String getProdKey() {
-        return prodKey;
-    }
+	public String getProdKey() {
+		return prodKey;
+	}
 
-    public void setProdKey(String prodKey) {
-        this.prodKey = prodKey;
-    }
+	public void setProdKey(String prodKey) {
+		this.prodKey = prodKey;
+	}
 
-    public String getUserKey() {
-        return userKey;
-    }
+	public String getUserKey() {
+		return userKey;
+	}
 
-    public void setUserKey(String userKey) {
-        this.userKey = userKey;
-    }
+	public void setUserKey(String userKey) {
+		this.userKey = userKey;
+	}
 
-    public String getAddrKey() {
-        return addrKey;
-    }
+	public String getAddrKey() {
+		return addrKey;
+	}
 
-    public void setAddrKey(String addrKey) {
-        this.addrKey = addrKey;
-    }
+	public void setAddrKey(String addrKey) {
+		this.addrKey = addrKey;
+	}
 
-    public String getCardCompany() {
-        return cardCompany;
-    }
+	public String getCardCompany() {
+		return cardCompany;
+	}
 
-    public void setCardCompany(String cardCompany) {
-        this.cardCompany = cardCompany;
-    }
+	public void setCardCompany(String cardCompany) {
+		this.cardCompany = cardCompany;
+	}
 
-    public int getOrderPrice() {
-        return orderPrice;
-    }
+	public int getOrderPrice() {
+		return orderPrice;
+	}
 
-    public void setOrderPrice(int orderPrice) {
-        this.orderPrice = orderPrice;
-    }
+	public void setOrderPrice(int orderPrice) {
+		this.orderPrice = orderPrice;
+	}
 
-    public int getOrderCnt() {
-        return orderCnt;
-    }
+	public int getOrderCnt() {
+		return orderCnt;
+	}
 
-    public void setOrderCnt(int orderCnt) {
-        this.orderCnt = orderCnt;
-    }
+	public void setOrderCnt(int orderCnt) {
+		this.orderCnt = orderCnt;
+	}
 
-    public String getOrderDate() {
-        return orderDate;
-    }
+	public String getOrderDate() {
+		return orderDate;
+	}
 
-    public void setOrderDate(String orderDate) {
-        this.orderDate = orderDate;
-    }
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
 
-    public int getRefundYn() {
-        return refundYn;
-    }
-
-    public void setRefundYn(int refundYn) {
-        this.refundYn = refundYn;
-    }
-
-    public String getRefundReason() {
-        return refundReason;
-    }
-
-    public void setRefundReason(String refundReason) {
-        this.refundReason = refundReason;
-    }
-
-    public String getRefundKey() {
-        return refundKey;
-    }
-
-    public void setRefundKey(String refundKey) {
-        this.refundKey = refundKey;
-    }
-
-    public String getRefundDate() {
-        return refundDate;
-    }
-
-    public void setRefundDate(String refundDate) {
-        this.refundDate = refundDate;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductHistory [impUid=" + impUid + ", orderNo=" + orderNo + ", prodKey=" + prodKey + ", userKey="
-                + userKey + ", addrKey=" + addrKey + ", cardCompany=" + cardCompany + ", orderPrice=" + orderPrice
-                + ", orderCnt=" + orderCnt + ", orderDate=" + orderDate + ", refundYn=" + refundYn + ", refundReason="
-                + refundReason + ", refundKey=" + refundKey + ", refundDate=" + refundDate + "]";
-    }
+	@Override
+	public String toString() {
+		return "OrderHistory [impUid=" + impUid + ", orderNo=" + orderNo + ", prodKey=" + prodKey + ", userKey="
+				+ userKey + ", addrKey=" + addrKey + ", cardCompany=" + cardCompany + ", orderPrice=" + orderPrice
+				+ ", orderCnt=" + orderCnt + ", orderDate=" + orderDate + "]";
+	}
 
 }

d3d74bf7d2ec502559ad1e98fcd04250236b5d25
엄태호 <oum96@naver.com>
2025-01-08 16:50:38 +0900

    메인페이지 구현중. 서버 폴더 안 web.xml의 <welcome-file>index.jsp</welcome-file> 주석처리 해야 메인페이지 보임

---
+++ src/main/java/com/khedu/sooljura/main/controller/MainController.java
@@ -0,0 +1,43 @@
+package com.khedu.sooljura.main.controller;
+
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.khedu.sooljura.admin.model.vo.Product;
+import com.khedu.sooljura.main.model.service.MainService;
+
+@Controller
+public class MainController {
+
+	@Autowired
+	@Qualifier("mainService")
+	private MainService service;
+	
+	@GetMapping("/")
+    public String main(Model model) {
+		System.out.println("접속완");
+        ArrayList<Product> allProdList = service.getAllItems();
+        
+        ArrayList<Product> wineProdList = service.getWine();
+        
+        ArrayList<Product> wiskeyProdList = service.getWiskey();
+        
+        ArrayList<Product> brandyProdList = service.getBrandy();
+        ArrayList<Product> liqueurProdList = service.getLiqueur();
+        ArrayList<Product> sojuProdList = service.getSoju();
+        ArrayList<Product> etcProdList = service.getEtc();
+        
+        
+        model.addAttribute("brandyProdList", brandyProdList);
+        model.addAttribute("liqueurProdList", liqueurProdList);
+        model.addAttribute("etcProdList", etcProdList);
+        model.addAttribute("sojuProdList", sojuProdList);
+        model.addAttribute("wineProdList", wineProdList);
+        model.addAttribute("wiskeyProdList", wiskeyProdList);
+        return "main/mainPage";
+    }
+}

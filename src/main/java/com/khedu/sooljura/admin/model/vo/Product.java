471ef8da1a9e2d4489591182237756353ce036dc
UnEmotioneD <blackeagle10@icloud.com>
2025-01-11 10:59:39 +0900

    Product.java 에 higherCategory 추가

--- src/main/java/com/khedu/sooljura/admin/model/vo/Product.java
+++ src/main/java/com/khedu/sooljura/admin/model/vo/Product.java
@@ -23,181 +23,176 @@ public class Product {
     private String imgKey;
     private String imgNm;
     private String imgPath;
-    
+
+    private String higherCategory;
+
     public Product() {
         super();
     }
 
-	public Product(String prodKey, String prodNm, int prodPrice, String prodMaker, String prodOrigin, String prodIntro,
-			int prodCnt, String prodVol, String prodProof, String uploadDate, int tradingYn, String categoryKey,
-			ProductCategory productCategory, ArrayList<ProductImage> productImages, String imgKey, String imgNm,
-			String imgPath) {
-		super();
-		this.prodKey = prodKey;
-		this.prodNm = prodNm;
-		this.prodPrice = prodPrice;
-		this.prodMaker = prodMaker;
-		this.prodOrigin = prodOrigin;
-		this.prodIntro = prodIntro;
-		this.prodCnt = prodCnt;
-		this.prodVol = prodVol;
-		this.prodProof = prodProof;
-		this.uploadDate = uploadDate;
-		this.tradingYn = tradingYn;
-		this.categoryKey = categoryKey;
-		this.productCategory = productCategory;
-		this.productImages = productImages;
-		this.imgKey = imgKey;
-		this.imgNm = imgNm;
-		this.imgPath = imgPath;
-	}
+    public Product(String prodKey, String prodNm, int prodPrice, String prodMaker, String prodOrigin, String prodIntro, int prodCnt, String prodVol, String prodProof, String uploadDate, int tradingYn, String categoryKey, ProductCategory productCategory, ArrayList<ProductImage> productImages, String imgKey, String imgNm, String imgPath, String higherCategory) {
+        this.prodKey = prodKey;
+        this.prodNm = prodNm;
+        this.prodPrice = prodPrice;
+        this.prodMaker = prodMaker;
+        this.prodOrigin = prodOrigin;
+        this.prodIntro = prodIntro;
+        this.prodCnt = prodCnt;
+        this.prodVol = prodVol;
+        this.prodProof = prodProof;
+        this.uploadDate = uploadDate;
+        this.tradingYn = tradingYn;
+        this.categoryKey = categoryKey;
+        this.productCategory = productCategory;
+        this.productImages = productImages;
+        this.imgKey = imgKey;
+        this.imgNm = imgNm;
+        this.imgPath = imgPath;
+        this.higherCategory = higherCategory;
+    }
 
-	public String getProdKey() {
-		return prodKey;
-	}
+    public String getProdKey() {
+        return prodKey;
+    }
 
-	public void setProdKey(String prodKey) {
-		this.prodKey = prodKey;
-	}
+    public void setProdKey(String prodKey) {
+        this.prodKey = prodKey;
+    }
 
-	public String getProdNm() {
-		return prodNm;
-	}
+    public String getProdNm() {
+        return prodNm;
+    }
 
-	public void setProdNm(String prodNm) {
-		this.prodNm = prodNm;
-	}
+    public void setProdNm(String prodNm) {
+        this.prodNm = prodNm;
+    }
 
-	public int getProdPrice() {
-		return prodPrice;
-	}
+    public int getProdPrice() {
+        return prodPrice;
+    }
 
-	public void setProdPrice(int prodPrice) {
-		this.prodPrice = prodPrice;
-	}
+    public void setProdPrice(int prodPrice) {
+        this.prodPrice = prodPrice;
+    }
 
-	public String getProdMaker() {
-		return prodMaker;
-	}
+    public String getProdMaker() {
+        return prodMaker;
+    }
 
-	public void setProdMaker(String prodMaker) {
-		this.prodMaker = prodMaker;
-	}
+    public void setProdMaker(String prodMaker) {
+        this.prodMaker = prodMaker;
+    }
 
-	public String getProdOrigin() {
-		return prodOrigin;
-	}
+    public String getProdOrigin() {
+        return prodOrigin;
+    }
 
-	public void setProdOrigin(String prodOrigin) {
-		this.prodOrigin = prodOrigin;
-	}
+    public void setProdOrigin(String prodOrigin) {
+        this.prodOrigin = prodOrigin;
+    }
 
-	public String getProdIntro() {
-		return prodIntro;
-	}
+    public String getProdIntro() {
+        return prodIntro;
+    }
 
-	public void setProdIntro(String prodIntro) {
-		this.prodIntro = prodIntro;
-	}
+    public void setProdIntro(String prodIntro) {
+        this.prodIntro = prodIntro;
+    }
 
-	public int getProdCnt() {
-		return prodCnt;
-	}
+    public int getProdCnt() {
+        return prodCnt;
+    }
 
-	public void setProdCnt(int prodCnt) {
-		this.prodCnt = prodCnt;
-	}
+    public void setProdCnt(int prodCnt) {
+        this.prodCnt = prodCnt;
+    }
 
-	public String getProdVol() {
-		return prodVol;
-	}
+    public String getProdVol() {
+        return prodVol;
+    }
 
-	public void setProdVol(String prodVol) {
-		this.prodVol = prodVol;
-	}
+    public void setProdVol(String prodVol) {
+        this.prodVol = prodVol;
+    }
 
-	public String getProdProof() {
-		return prodProof;
-	}
+    public String getProdProof() {
+        return prodProof;
+    }
 
-	public void setProdProof(String prodProof) {
-		this.prodProof = prodProof;
-	}
+    public void setProdProof(String prodProof) {
+        this.prodProof = prodProof;
+    }
 
-	public String getUploadDate() {
-		return uploadDate;
-	}
+    public String getUploadDate() {
+        return uploadDate;
+    }
 
-	public void setUploadDate(String uploadDate) {
-		this.uploadDate = uploadDate;
-	}
+    public void setUploadDate(String uploadDate) {
+        this.uploadDate = uploadDate;
+    }
 
-	public int getTradingYn() {
-		return tradingYn;
-	}
+    public int getTradingYn() {
+        return tradingYn;
+    }
 
-	public void setTradingYn(int tradingYn) {
-		this.tradingYn = tradingYn;
-	}
+    public void setTradingYn(int tradingYn) {
+        this.tradingYn = tradingYn;
+    }
 
-	public String getCategoryKey() {
-		return categoryKey;
-	}
+    public String getCategoryKey() {
+        return categoryKey;
+    }
 
-	public void setCategoryKey(String categoryKey) {
-		this.categoryKey = categoryKey;
-	}
+    public void setCategoryKey(String categoryKey) {
+        this.categoryKey = categoryKey;
+    }
 
-	public ProductCategory getProductCategory() {
-		return productCategory;
-	}
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
 
-	public void setProductCategory(ProductCategory productCategory) {
-		this.productCategory = productCategory;
-	}
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
 
-	public ArrayList<ProductImage> getProductImages() {
-		return productImages;
-	}
+    public ArrayList<ProductImage> getProductImages() {
+        return productImages;
+    }
 
-	public void setProductImages(ArrayList<ProductImage> productImages) {
-		this.productImages = productImages;
-	}
+    public void setProductImages(ArrayList<ProductImage> productImages) {
+        this.productImages = productImages;
+    }
 
-	public String getImgKey() {
-		return imgKey;
-	}
+    public String getImgKey() {
+        return imgKey;
+    }
 
-	public void setImgKey(String imgKey) {
-		this.imgKey = imgKey;
-	}
+    public void setImgKey(String imgKey) {
+        this.imgKey = imgKey;
+    }
 
-	public String getImgNm() {
-		return imgNm;
-	}
+    public String getImgNm() {
+        return imgNm;
+    }
 
-	public void setImgNm(String imgNm) {
-		this.imgNm = imgNm;
-	}
+    public void setImgNm(String imgNm) {
+        this.imgNm = imgNm;
+    }
 
-	public String getImgPath() {
-		return imgPath;
-	}
+    public String getImgPath() {
+        return imgPath;
+    }
 
-	public void setImgPath(String imgPath) {
-		this.imgPath = imgPath;
-	}
+    public void setImgPath(String imgPath) {
+        this.imgPath = imgPath;
+    }
 
-	@Override
-	public String toString() {
-		return "Product [prodKey=" + prodKey + ", prodNm=" + prodNm + ", prodPrice=" + prodPrice + ", prodMaker="
-				+ prodMaker + ", prodOrigin=" + prodOrigin + ", prodIntro=" + prodIntro + ", prodCnt=" + prodCnt
-				+ ", prodVol=" + prodVol + ", prodProof=" + prodProof + ", uploadDate=" + uploadDate + ", tradingYn="
-				+ tradingYn + ", categoryKey=" + categoryKey + ", productCategory=" + productCategory
-				+ ", productImages=" + productImages + ", imgKey=" + imgKey + ", imgNm=" + imgNm + ", imgPath="
-				+ imgPath + "]";
-	}
-	
-	
+    public String getHigherCategory() {
+        return higherCategory;
+    }
+
+    public void setHigherCategory(String higherCategory) {
+        this.higherCategory = higherCategory;
+    }
 
 }
